6cc2547982f21228e1342116f8092309a704b0b9
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2025-08-02 10:34:38 -0300

    Add initial flake configuration and .gitignore file
    - Created a .gitignore file to exclude the /target directory.
    - Added flake.nix to define a Rust development environment with necessary tools.
    - Generated flake.lock to lock dependencies for the project.

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "RegExp Extract DataFusion - Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            rust-analyzer
+            clippy
+            cargo-machete
+            cargo-edit
+          ];
+
+          shellHook = ''
+            echo "🦀 Rust development environment loaded"
+            echo "Available tools:"
+            echo "  rustc --version"
+            echo "  cargo --version" 
+            echo "  rustfmt --version"
+            echo "  rust-analyzer --version"
+            echo "  cargo clippy --version"
+            echo "  cargo machete --version"
+            echo "  cargo edit --version"
+          '';
+        };
+      });
+} 
